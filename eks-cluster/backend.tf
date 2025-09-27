42e472c31817f4a4947ada7a3e792c0d7ca97c93
ssgaik752 <127913640+ssgaik752@users.noreply.github.com>
2025-09-27 16:26:51 +0530

    Update backend.tf

--- eks-cluster/backend.tf
+++ eks-cluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jensins-kubernetes-app-2025-v3"
+    bucket = "jenkins-terraform-kubernetes-app-2025-v3"
     region = "ap-south-1"
     key    = "eks/terraform.tfstate"
   }
